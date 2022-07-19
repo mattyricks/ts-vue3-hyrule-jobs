50d70c2dffa8ab45d69b582deb4e36bcbb265ed1
Matthew <ricklefsmatthew1003@gmail.com>
2022-07-19 12:35:19 -0500

    introduces reactive setup, refs, and generic typing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="app">
-    <p>
-      {{ name }}
-    </p>
+    <p>{{ name }} - {{ age }}</p>
 
     <button @click="changeName('Zelda')">change name</button>
+
+    <button @click="changeAge(35)">change name</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, ref, toRefs } from "vue";
 
 export default defineComponent({
   name: "App",
   components: {},
-  data() {
-    return {
-      name: "Link",
-      age: 25 as number | string,
-    };
+  setup() {
+    // const state = reactive({
+    //   name: "Link",
+    //   age: 25 as string | number,
+    // });
+
+    // state.name = "Matty";
+
+    // return { ...toRefs(state) };
+
+    const name = ref("Link");
+    const age = ref<number | string>(32);
   },
+
   methods: {
     changeName(name: string) {
       this.name = name;
+      return name;
+    },
+    changeAge(age: number | string) {
+      this.age = age;
+      return age;
     },
   },
 });
