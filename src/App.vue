c7a18acbbda7462427d4183f435ae31e3ea1a28c
Matthew <ricklefsmatthew1003@gmail.com>
2022-07-21 11:30:27 -0500

    creates JobList component, adds jobs array to App.vue, passes as props to JobList component, styles JobList elements

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,47 @@
 <template>
-  <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-
-    <button @click="changeName('Zelda')">change name</button>
-
-    <button @click="changeAge(35)">change name</button>
-  </div>
+  <div class="app"><JobList :jobs="jobs" /></div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from "vue";
+import { defineComponent, ref } from "vue";
+import Job from "./types/Job";
+import JobList from "./components/JobList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
-    // const state = reactive({
-    //   name: "Link",
-    //   age: 25 as string | number,
-    // });
+    const jobs = ref<Job[]>([
+      {
+        title: "farmworker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
 
-    // state.name = "Matty";
-
-    // return { ...toRefs(state) };
-
-    const name = ref("Link");
-    const age = ref<number | string>(32);
-  },
-
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-      return name;
-    },
-    changeAge(age: number | string) {
-      this.age = age;
-      return age;
-    },
+    return {
+      jobs,
+    };
   },
 });
 </script>
